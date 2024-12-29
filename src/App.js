5f36c643010fdf3f25ebe73bbb98f6153e759012
Adam Nowak <nowak.adam2222@gmail.com>
2024-12-29 18:27:15 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <header>
+      osu!std DAN courses
+    </header>
+    <YourProfile/>
+    <div>
+      <MapDownload/>
+      <MapDownload/>
+      <MapDownload/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/YourProfile/YourProfile.js
@@ -0,0 +1,5 @@
+const YourProfile = () => {
+    
+};
+
+export default YourProfile;
